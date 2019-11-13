5befbed9eb6ff231a7ca830082a708b0713a2f16
Robert Hanna <robertdhanna94@gmail.com>
2019-11-13 16:30:03 -0700

    DRY it up

--- src/Scenery.cpp
+++ src/Scenery.cpp
@@ -15,18 +15,23 @@ void BackGround::update(double delta)
     this->event_bus->publish_render_event(Events::createRenderTextureEvent(this->texture_index, this->clip, this->position, scale, 0u));
 }
 
+void render_if_on_screen(EventBus *event_bus, size_t texture_index, V2 &position, Rect &clip, size_t scale)
+{
+    Rect camera = Camera::get_camera();
+    V2 render_position = {position.x - camera.x, position.y - camera.y};
+    Rect box = {position.x, position.y, static_cast<int>(clip.w * scale), static_cast<int>(clip.h * scale)};
+    if (Physics::checkCollision(camera, box))
+    {
+        event_bus->publish_render_event(Events::createRenderTextureEvent(texture_index, clip, render_position, scale, 1u));
+    }
+};
+
 Dirt::Dirt(EventBus *e, const Rect &clip, const V2 &position, size_t texture_index, size_t scale)
     : Scenery(e, clip, position, texture_index, scale){};
 
 void Dirt::update(double delta)
 {
-    Rect camera = Camera::get_camera();
-    V2 render_position = {this->position.x - camera.x, this->position.y - camera.y};
-    Rect box = {this->position.x, this->position.y, static_cast<int>(this->clip.w * this->scale), static_cast<int>(this->clip.h * this->scale)};
-    if (Physics::checkCollision(camera, box))
-    {
-        this->event_bus->publish_render_event(Events::createRenderTextureEvent(this->texture_index, this->clip, render_position, this->scale, 1u));
-    }
+    render_if_on_screen(this->event_bus, this->texture_index, this->position, this->clip, this->scale);
 }
 
 Tree::Tree(EventBus *e, const Rect &clip, const V2 &position, size_t texture_index, size_t scale)
@@ -34,11 +39,5 @@ Tree::Tree(EventBus *e, const Rect &clip, const V2 &position, size_t texture_ind
 
 void Tree::update(double delta)
 {
-    Rect camera = Camera::get_camera();
-    V2 render_position = {this->position.x - camera.x, this->position.y - camera.y};
-    Rect box = {this->position.x, this->position.y, static_cast<int>(this->clip.w * this->scale), static_cast<int>(this->clip.h * this->scale)};
-    if (Physics::checkCollision(camera, box))
-    {
-        this->event_bus->publish_render_event(Events::createRenderTextureEvent(this->texture_index, this->clip, render_position, this->scale, 1u));
-    }
+    render_if_on_screen(this->event_bus, this->texture_index, this->position, this->clip, this->scale);
 }
