ca228578272e983994b5a353bd86528f556673d9
Robert Hanna <robertdhanna94@gmail.com>
2019-11-17 14:21:47 -0700

    Oops. Anchor switchup.

--- src/Console.cpp
+++ src/Console.cpp
@@ -4,8 +4,8 @@ Console::Console(EventBus *e) : event_bus(e)
 {
     // init UIPanel
     this->console_panel.dimensions = {400, 100};
-    this->console_panel.anchor_horizontal = {AnchorType::TOP, 0};
-    this->console_panel.anchor_vertical = {AnchorType::LEFT, 5};
+    this->console_panel.anchor_horizontal = {AnchorType::LEFT, 0};
+    this->console_panel.anchor_vertical = {AnchorType::TOP, 5};
     Text t = Text(e, 0, "console-text");
     t.set_text("Yeet");
     this->console_panel.panel_text.push_back(t);
